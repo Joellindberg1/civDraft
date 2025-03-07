3906b67d5dc7a6a9ae1a864f67e19b3afd6a6ec3
[Joel LIndberg] <[Joellindberg23@gmail.com]>
2025-03-07 01:02:35 +0100

    Clean up and map  structure

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+//React
+// import { useState } from 'react'
+//Components
+
+//Styles
+import './App.scss'
+//Pages
+
+//Types
+
+//Interfaces
+
+
+function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
